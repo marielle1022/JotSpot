b789be76160c9a032ea5446436397cb1c565a117
marielle1022 <marielle.riveros@gmail.com>
2021-04-11 14:16:24 -0400

    created activity for past entries

---
+++ app/src/main/java/edu/neu/madcourse/jotspot/past_entries/PastEntriesActivity.java
@@ -0,0 +1,16 @@
+package edu.neu.madcourse.jotspot.past_entries;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.neu.madcourse.jotspot.R;
+
+public class PastEntriesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_past_entries);
+    }
+}
